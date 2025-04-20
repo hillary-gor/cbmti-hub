0c2627795a98c1460ab0a34631125ea1195eaa1d
Hillary Gor <125491337+hillary-se@users.noreply.github.com>
2025-04-20 16:22:56 +0300

    Update app/layout.tsx: - add description to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "CBMTI Hub",
-  description: "",
+  description: "Empowering Healthcare Students Across Kenya",
 };
 
 export default function RootLayout({
