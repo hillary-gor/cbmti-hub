365e8ce4b246e9c8d948481b091a8b48edcd5e1d
Hillary Gor <125491337+hillary-se@users.noreply.github.com>
2025-07-24 21:57:21 +0300

    - Allow unused parameters that start with `_`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Allow unused parameters that start with `_`
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }
 ];
 
 export default eslintConfig;
